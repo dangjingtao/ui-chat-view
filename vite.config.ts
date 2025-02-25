3308cabd81edf3f665e32a85f336e4cd4522c093
Tomz <dangjingtao@163.com>
2025-02-25 09:54:56 +0800

    feat: add fusejs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 import Components from "unplugin-vue-components/vite";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+// 读取 package.json 文件
+const pkg = JSON.parse(
+  readFileSync(resolve(__dirname, "package.json"), "utf-8"),
+);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +24,11 @@ export default defineConfig({
     }),
     Icons(),
   ],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+    __APP_NAME__: JSON.stringify(pkg.name),
+    __APP_DESCRIPTION__: JSON.stringify(pkg.description),
+  },
   server: {
     port: 8461,
   },
@@ -25,5 +37,9 @@ export default defineConfig({
       "@": path.resolve("./src"),
       "#": path.resolve("./types"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
 });
