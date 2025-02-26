d020e18bb650445b370312dcbf8e24853bbd38da
Tomz <dangjingtao@163.com>
2025-02-26 19:59:00 +0800

    feat: test postcss

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import { createApp } from "vue";
 import router from "@/router";
 import cachePlugin from "@/plugins/cachePlugin/index";
 import { MdPreview } from "md-editor-v3";
-import "./style.css";
 import App from "./App.vue";
+import "./style.css";
 
 cachePlugin.install().then(() => {
   createApp(App).use(router).use(MdPreview).mount("#app");
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,8 @@
 
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
